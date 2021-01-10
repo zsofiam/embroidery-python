a6e9cfa52ae240db31c23005fb1615227286d446
matyasi.zsofia <zsofia.matyasi@gmail.com>
2021-01-10 12:34:53 +0100

    implement draw_rectangle function

--- embroidery.py
+++ embroidery.py
@@ -1,5 +1,8 @@
-def draw_rectangle(width, height):
-    matrix = []
+def draw_rectangle(width, height, border_color=1, fill_color=1, border_width=1):
+    matrix = [[border_color for column in range(width)] for row in range(height)]
+    for row in range(border_width, height - border_width):
+        for column in range(border_width, width - border_width):
+            matrix[row][column] = fill_color
     return matrix
 
 
@@ -29,6 +32,7 @@ def embroider(matrix, color_scheme):
 if __name__ == '__main__':
     color_scheme = {0: ' ', 1: '*', 2: '.'}
     embroider([[0, 0, 0, 1, 0, 0, 0], [0, 0, 1, 2, 1, 0, 0], [0, 1, 2, 2, 2, 1, 0], [1, 1, 1, 1, 1, 1, 1]], color_scheme)
+    embroider(draw_rectangle(6, 6, 1, 2, 1), color_scheme)
 
     # This should have the same output:
     # embroider(draw_triangle(4, border_color=1, fill_color=2), color_scheme)
